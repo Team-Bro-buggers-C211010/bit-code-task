459d1d53a9efe6f407af0fbd44084f88baf778d3
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-06-15 15:11:28 +0600

    roadmap route created

--- server/src/routes/roadmapRoutes.js
+++ server/src/routes/roadmapRoutes.js
@@ -1,4 +1,18 @@
 import express from 'express';
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.json({ message: 'Get all roadmap items' });
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `Specific roadmap item with ID: ${id}` });
+});
+
+router.put('/:id/upvote', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `Upvoted a roadmap item with ID: ${id}` });
+});
+
 export default router;
