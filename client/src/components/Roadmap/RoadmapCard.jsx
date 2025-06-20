2a064fb879c7c2d3c0d5e8a876bad5daef794c39
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-06-20 12:11:24 +0600

    dynamically showed roadmap card

--- client/src/components/Roadmap/RoadmapCard.jsx
+++ client/src/components/Roadmap/RoadmapCard.jsx
@@ -1,27 +1,27 @@
 import { AiOutlineLike } from "react-icons/ai";
-const RoadmapCard = () => {
+const RoadmapCard = ({ roadmap }) => {
     return (
         <div className="bg-white rounded-lg border border-gray-200 p-6 shadow hover:shadow-lg transition">
             <div className="flex justify-between items-center mb-3">
                 <span className="text-sm bg-yellow-100 text-yellow-700 px-2 py-1 rounded font-medium">
-                    Feature
+                    {roadmap.category}
                 </span>
                 <span className="text-xs bg-blue-100 text-blue-700 px-2 py-1 rounded font-semibold">
-                    In Progress
+                    {roadmap.status}
                 </span>
             </div>
 
             <h3 className="text-xl font-semibold text-slate-900 mb-2">
-                Improve UI Consistency
+                {roadmap.title}
             </h3>
 
             <p className="text-sm text-gray-600 mb-4">
-                Align all input fields and spacing across the roadmap module to ensure visual consistency.
+                {roadmap.description.length > 100 ? `${roadmap.description.slice(0, 100)}...` : roadmap.description}
             </p>
 
             <div className="flex justify-between items-center">
                 <p className="flex items-center gap-2 text-sm px-3 py-1.5 border border-yellow-400 text-yellow-500 rounded">
-                    <button><AiOutlineLike className="text-neutral-950 hover:text-yellow-400 hover:font-semibold text-lg cursor-pointer transition-colors duration-200" /></button>12
+                    <button><AiOutlineLike className="text-neutral-950 hover:text-yellow-400 hover:font-semibold text-lg cursor-pointer transition-colors duration-200" /></button>{roadmap.upvotesCount}
                 </p>
                 <span className="text-sm text-blue-600 hover:underline cursor-pointer">
                     View Details
--- client/src/pages/Roadmap.jsx
+++ client/src/pages/Roadmap.jsx
@@ -1,12 +1,20 @@
+import { useEffect } from "react";
 import RoadmapCard from "../components/Roadmap/RoadmapCard";
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllRoadmaps } from "../features/Roadmaps/roadmapThunk";
 
 const Roadmap = () => {
+  const { roadmaps } = useSelector((state) => state.roadmap);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllRoadmaps());
+  },[dispatch]);
   return (
     <div className="max-w-7xl mx-auto px-4 py-10">
       <h2 className="text-3xl font-bold mb-8 text-slate-900">All Roadmaps</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {[1, 2, 3].map((_, i) => (
-          <RoadmapCard key={i} />
+        {roadmaps.map((roadmap, i) => (
+          <RoadmapCard roadmap={roadmap} key={i} />
         ))}
       </div>
     </div>
