ac80e905b13793f04149c5a67a3b31bf16f7b063
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-06-23 14:05:23 +0600

    server cookie setting

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -7,7 +7,7 @@ export const generateToken = (userId, res) => {
     res.cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "none",
         secure: process.env.NODE_ENV === "production"
     });
 
